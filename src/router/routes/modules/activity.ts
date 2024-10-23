7d688bf68dd1bc26f1e132ffbb91216b8e0d8d2c
guoshuai%40hanspro.cn <gs980328>
2024-10-23 21:29:45 +0800

    修改一起野router

--- src/router/routes/modules/activity.ts
+++ src/router/routes/modules/activity.ts
@@ -5,11 +5,13 @@ const DASHBOARD: AppRouteRecordRaw = {
     path: '/activity',
     name: 'activity',
     component: DEFAULT_LAYOUT,
+    redirect: '/activity/wildTogether',
     meta: {
         locale: '一起野管理',
         requiresAuth: true,
         icon: 'icon-bookmark',
         order: 3,
+        hideChildrenInMenu: true,
     },
     children: [
         {
